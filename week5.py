a465798a688e3077b7e324db8e3c52abf177a198
BerkantDuman <dumanberkant562@gmail.com>
2019-03-12 17:59:55 +0300

    5. hafta

--- week5.py
+++ week5.py
@@ -1,5 +1,6 @@
 from math import sqrt
 sayi=14
+
 def sayi_asal_mi(sayi):
     i=3
 
@@ -21,12 +22,13 @@ def sayi_asal_carpan(sayi):
     else:
         for i in range(2,sayi+1):
             if (sayi % i == 0):
-                if(sayi_asal_mi(i)):
-                    liste.append(i)
+                #if(sayi_asal_mi(i)):
+               liste.append(i)
 
         return liste
 print(sayi_asal_carpan(sayi))
 liste=[4,-3,2,-1,-2,6,-5]
+
 def liste_ardasik_toplam(liste):
     dizi=[]
     i=2
@@ -36,3 +38,27 @@ def liste_ardasik_toplam(liste):
         i+=1
     return max(dizi)
 print(liste_ardasik_toplam(liste))
+
+#dynamic yöntem
+def para_ustu_hesaplama(N,paralar,knownResult):
+    minCoin=N
+    if N in paralar:
+        knownResult[N]=1
+        return 1
+
+    elif(knownResult[N] > 0):
+        return knownResult[N]
+
+    else:
+        for i in [c  for c in paralar if c <= N]: #expression
+            numCoin= 1 + para_ustu_hesaplama(N-i, paralar,knownResult)
+            if (numCoin < minCoin):
+                minCoin=numCoin
+                knownResult[N]=minCoin
+    return minCoin
+
+
+paraUstu=63
+paralar=[1,5,10,25,50]
+i=0
+print(para_ustu_hesaplama(paraUstu,paralar,[0]*(paraUstu+1)))
